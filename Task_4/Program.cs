18fd0fcd8523300cc4f5cada59ac71212ee466fc
lokman <kalinin5000@yandex.ru>
2022-11-30 22:13:31 +0300

    решение четвертой задачи

---
+++ Task_4/Program.cs
@@ -0,0 +1,39 @@
+﻿int n = 1;
+while (n < 101)
+{
+    if (n % 3 == 0)
+    {
+        if (n % 5 == 0)
+        {
+            Console.Write("FizzBuzz ");
+            n++;
+            continue;
+        }
+        else
+        {
+            Console.Write("Fizz ");
+            n++;
+            continue;
+        }
+    }
+    if (n % 5 == 0)
+    {
+        if (n % 3 == 0)
+        {
+            Console.Write("FizzBuzz ");
+            n++;
+            continue;
+        }
+        else
+        {
+            Console.Write("Buzz ");
+            n++;
+            continue;
+        }
+    }
+    else
+    {
+        Console.Write(n + " ");
+        n++;
+    }
+}
