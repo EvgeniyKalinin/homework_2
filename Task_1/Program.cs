065963fd059165be780ed22c7bd9a0dfe52f1a64
lokman <kalinin5000@yandex.ru>
2022-11-30 21:29:34 +0300

    решение первой задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+if (100 > number)
+{
+    Console.WriteLine("Введено некорректное число");
+}
+else if (number > 999)
+{
+    Console.WriteLine("Введено некорректное число");
+}
+else
+{
+    number = number / 10 % 10;
+    Console.WriteLine("Вторая цифра числа " + number + ".");
+}
