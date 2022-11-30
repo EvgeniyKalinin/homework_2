28c1393d28ddd9fd630eef31298b6735d4994e1a
lokman <kalinin5000@yandex.ru>
2022-11-30 21:32:30 +0300

    решение второй задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число.");
+  int number = Convert.ToInt32(Console.ReadLine());
+  while (number > 1000)
+  {
+   number /= 10;
+  }
+  if (number < 100) 
+  {
+   Console.WriteLine("Третьей цифры нет");
+  }
+  else 
+  {
+  number = number % 10;
+  Console.WriteLine("Третья цифра числа " + number + ".");
+  }
