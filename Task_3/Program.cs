ea1966a1954687faf7d65db53f2db15d334b180d
lokman <kalinin5000@yandex.ru>
2022-11-30 21:38:56 +0300

    решение третей задачи

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    if (number <= 7)
+    {
+        if (number <= 5)
+        {
+            Console.WriteLine("Будний день.");
+        }
+        else if (number > 5)
+        {
+            Console.WriteLine("Выходной день.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное число.");
+    }
+}
+else
+{
+    Console.WriteLine("Введено некорректное число.");
+}
