7dc11b60e4b1bd24c3d719243e0cf9b0cdcd0863
lokman <kalinin5000@yandex.ru>
2022-11-30 22:32:00 +0300

    решение пятой задачи

---
+++ Task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = number % 10;
+if (x == 1)
+{
+    Console.WriteLine($"В комнате {number} програмист.");
+}
+else if (x == 2 | x == 3 | x == 4)
+{
+    Console.WriteLine($"В комнате {number} програмиста.");
+}
+else if (x == 5 | x == 6 | x == 7 | x == 8 | x == 9 | x == 0)
+{
+    Console.WriteLine($"В комнате {number} програмистов.");
+}
